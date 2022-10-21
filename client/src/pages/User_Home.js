37d1db2de31a580150e4c8f4dcde3d474a6e8c84
Ishan Mody <ishan.mody@gmail.com>
2022-10-20 20:58:34 -0400

    some visual changese to tutorHome

--- client/src/pages/User_Home.js
+++ client/src/pages/User_Home.js
@@ -1,6 +1,9 @@
 import {useState} from 'react';
 import { useLocation, useNavigate } from "react-router-dom";
 import Calendar from 'react-calendar';
+import '../styles/Tutorhome.css'
+import { Sidebar, Menu, MenuItem, useProSidebar, SubMenu} from 'react-pro-sidebar';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 
 function UserHome(props) {    
     const location = useLocation();
@@ -8,9 +11,27 @@ function UserHome(props) {
     console.log('userdata: ', userData);
     console.log('props', props);
     const [value, onChange] = useState(new Date());
+    const { collapseSidebar } = useProSidebar();
 
     return (
-        <div>
+        <div className='home_container'>
+            <div className='sidebar'>                
+                <Sidebar backgroundColor='rgb(45, 207, 12, 0.5)'>
+                    <Menu closeOnClick='true'>                       
+                        <MenuItem> Upcoming Sessions</MenuItem>
+                        <MenuItem> Schedule a Tutoring Session</MenuItem>
+                        <MenuItem> My Profile </MenuItem>                        
+                        <SubMenu label="Help">
+                            <MenuItem className='help'> To report an issue or to ask a question, please email support@razortutor.com. We will reply ASAP. </MenuItem>                        
+                        </SubMenu>
+                    </Menu>                  
+                </Sidebar>                                    
+            </div>
+            <main>                    
+                <button className = 'collapse' onClick={() => collapseSidebar()}>
+                    <ArrowLeftIcon />
+                </button>
+            </main>
             <h1>Hi, {userData.user.first_name} see your upcoming sessions</h1>
             <Calendar 
                 tileClassName={({ date, view }) => {if(date.getUTCDate() === 25){
