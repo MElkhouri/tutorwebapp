5daf608349fcf700cfe5431e39009705efb8482d
Mike Elk <michaelelkhouri2019@gmail.com>
2022-11-07 19:08:38 -0500

    userhome and apply fixes

---
+++ client/src/pages/Apply.js
@@ -0,0 +1,68 @@
+import React, {useRef, useState} from "react";
+import emailjs from 'emailjs-com'
+import { Navigate } from "react-router-dom";
+
+
+
+
+function Apply() {
+
+ // const [name, setName] = useState("");
+  const form = useRef();
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qgy7z78', 'template_oznpx1r', form.current, 'PKWK63FQmkCdYEeqO')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      //Navigate("/home")
+  };
+
+  const handleFileInput = (e) => {
+    console.log("handlefileinput handler");
+    console.log(e.target.files[0]);
+   // console.log(setSelectedFile(e.target.files[0]));
+    console.log(selectedFile);
+    function getExtension(filename) {
+        console.log("in helper function");
+        return filename.split('.').pop();
+    };
+    //error here add a way of checking filetype. Not sure why some logs are not coming through.
+    console.log("before if");
+    console.log("hi " + selectedFile.name);
+
+    if( !(getExtension(selectedFile.name).toLowerCase() === "pdf" 
+    || getExtension(selectedFile.name).toLowerCase() === "docx") ){
+      console.log("after if");
+      }
+    console.log("after if but outside");
+
+  }
+
+  return (
+    <div className="login form">
+    <span>Become a tutor</span>
+    <h3>If you're interested in being a tutor, please fill out the information below and attach your most recent resume. You will be sent an email within 48 hours containing the next steps. Pay starts at $18/hr and varies by experience.</h3>
+    <form ref={form} onSubmit={sendEmail}>
+      
+      <input type="text" required name="first_name" placeholder="First Name" />
+     
+      <input type="text" required name="last_name" placeholder="Last Name" />
+     
+      <input type="email" required name="user_email" placeholder="Enter your email address"/>
+      <input type="text" requierd name="phone" placeholder="Phone Number"/>
+      <input type = "file" required value = {selectedFile} onChange = {handleFileInput}/>
+      <button type="submit">Apply</button>
+    </form>
+    </div>
+  );
+
+
+}
+
+export default Apply;
--- client/src/pages/User_Home.js
+++ client/src/pages/User_Home.js
@@ -2,18 +2,22 @@ import {useState} from 'react';
 import { useLocation, useNavigate } from "react-router-dom";
 import Calendar from 'react-calendar';
 import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
 import '../styles/Tutorhome.css'
 
-function UserHome(props) {    
+
+
+function UserHome(props){    
     const location = useLocation();
     const [userData] = useState(location.state);
+
     console.log('userdata: ', userData);
     console.log('props', props);
     const [value, onChange] = useState(new Date());
 
     return (
         <div className='home_container'>
-            <Sidebar user = {userData} />
+            <Sidebar user = {userData.user} />
             <div className='home_body'>
                 <h1 className='title'>Hi, {userData.user.first_name} see your upcoming sessions</h1>
                 <Calendar className='calendar' tileClassName={({ date, view }) => {if(date.getUTCDate() === 25){ return 'highlight'; }}} onChange={onChange} value={value} />
