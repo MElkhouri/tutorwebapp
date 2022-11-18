34553eb282a2149f92b1e56573bdbb038d184932
Ishan Mody <ishan.mody@gmail.com>
2022-11-18 15:23:10 -0500

    added all image assests

--- server/routes/Appointments.js
+++ server/routes/Appointments.js
@@ -1,20 +1,33 @@
+const { request } = require('express');
+const e = require('express');
 const express = require('express');
 const router = express.Router();
 const {Appointments} = require("../models");
+const { Op } = require("sequelize");
 
 router.get("/", async (req, res) => { //
     const listOfAppointments = await Appointments.findAll();
     res.json(listOfAppointments);
 });
-
+router.post("/getUserAppointments", async (req, res) => {
+    const allAppointments = await Appointments.findAll({
+        where: {student: req.body.student, date: {[Op.gte]: req.body.date}}
+    });
+    console.log("all APP: ", allAppointments);
+    if(allAppointments.length > 0){
+        res.send(allAppointments);
+    } else {
+        res.send("No appointments");
+    }
+});
 router.post("/createAppointment", async (req, res) => {
 
     console.log("Request: ", req.body);
     let temp = new Date(req.body.date);
-    temp.setTime(temp.getTime() - new Date().getTimezoneOffset() * 60 * 1000);
+    temp.setTime(temp.getTime());
 	let date = temp;
     const[row, created] = await Appointments.findOrCreate({ //authenticates registration
-        where: {tutor: req.body.tutor, student: req.body.student}, //user wont be able to create a bad appointment, tutor the same depending on future debate
+        where: {tutor: req.body.tutor, student: req.body.student, date:{[Op.eq]: date}}, //user wont be able to create a bad appointment, tutor the same depending on future debate
         defaults: {date: date, tutor: req.body.tutor, student: req.body.student}
     });
     if(created){
